1c449bcbcf4c55a30a5624efd3c0935912c59a2d
Anirban166 <bloodraven166@gmail.com>
2021-01-22 20:21:47 +0530

    Add shell scripts with paths to clear residuals of Microsoft autoupdate and Huawei drivers

---
+++ removeHuaweiStuff.sh
@@ -0,0 +1,13 @@
+# Files:
+# Launch Agents
+/Library/LaunchAgents/HWPortCfg.plist
+/Library/LaunchAgents/ouc.plist
+
+# Directories:
+# Startup Items
+/Library/StartupItems/HWNetMgr
+/Library/StartupItems/HWPortDetect
+/Library/StartupItems/StartOuc
+
+# Reboot System after cleaning initials:
+sudo shutdown -r 
---
+++ removeMicrosoftStuff.sh
@@ -0,0 +1,12 @@
+# Relevant paths to search for in /Library:
+/Library/LaunchAgents/
+/Library/PrivelegedHelperTools/ 
+/Library/Preferences/
+/Library/Saved Application State/
+# For most of the files, the pattern is 'com.microsoft.autoupdate'.
+
+# Folders:
+/Library/ApplicationSupport/Microsoft/MAU2
+/Library/ApplicationSupport/Microsoft AU Daemon
+
+
